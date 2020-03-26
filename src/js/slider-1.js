f37414080f13a8e1cd73d2c0234d59c775c20d28
Maksimilian1984 <58360374+Maksimilian1984@users.noreply.github.com>
2020-03-26 16:23:37 +0300

    откорректировал блок reviews, надо както оживить слайдер

--- src/js/slider-1.js
+++ src/js/slider-1.js
@@ -2,30 +2,36 @@
 const slider1Toggle1 = document.querySelector('.slider-1__toggle-input-1');
 const slider1Toggle2 = document.querySelector('.slider-1__toggle-input-2');
 const slider1Toggle3 = document.querySelector('.slider-1__toggle-input-3');
+const slider1Button1200 = document.querySelector('.slider__button-1200--1');
+const slider2Button1200 = document.querySelector('.slider__button-1200--2');
+const reviewsWrapper = document.querySelector('.reviews__wrapper');
 
-const reviewsItem1 = document.querySelector('.reviews__item-1');
-const reviewsItem2 = document.querySelector('.reviews__item-2');
-const reviewsItem3 = document.querySelector('.reviews__item-3');
 
-if (slider1Toggle1.checked) {
-    reviewsItem1.classList.add('slide-on');
-} else if (slider1Toggle2.checked) {
-    reviewsItem2.classList.add('slide-on');
-} else if (slider1Toggle3.checked) {
-    reviewsItem3.classList.add('slide-on')}
+
 
 slider1Toggle2.addEventListener('change', function () {
-    reviewsItem2.classList.add('slide-on');
-    reviewsItem1.classList.remove('slide-on');
-    reviewsItem3.classList.remove('slide-on');
-});
-slider1Toggle1.addEventListener('change', function () {
-    reviewsItem2.classList.remove('slide-on');
-    reviewsItem1.classList.add('slide-on');
-    reviewsItem3.classList.remove('slide-on');
+    reviewsWrapper.classList.add('reviews__wrapper--1');
+    reviewsWrapper.classList.remove('reviews__wrapper--2');
+
 });
 slider1Toggle3.addEventListener('change', function () {
-    reviewsItem2.classList.remove('slide-on');
-    reviewsItem1.classList.remove('slide-on');
-    reviewsItem3.classList.add('slide-on');
+    reviewsWrapper.classList.add('reviews__wrapper--2');
+    reviewsWrapper.classList.remove('reviews__wrapper--1');
+
 });
+slider1Toggle1.addEventListener('change', function () {
+    reviewsWrapper.classList.remove('reviews__wrapper--1');
+    reviewsWrapper.classList.remove('reviews__wrapper--2');
+
+});
+
+slider1Button1200.addEventListener('click', function () {
+    reviewsWrapper.style.transform = 'translateX(0 px)';
+
+})
+
+slider2Button1200.addEventListener('click', function () {
+
+    reviewsWrapper.style.transform = 'translateX(-1200px)'
+
+})
